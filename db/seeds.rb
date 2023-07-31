4e6ec79b4de6a1ea0da023744b78b1bbc7932fac
Edouard <De Montchenu>
2023-07-31 16:05:01 +0200

    beer show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts 'Creating DB'
-Beer.create!(name: 'HK', country: 'Netherlands', style: 'Pils', abv: 4.5)
-Beer.create!(name: 'Kro', country: 'France', style: 'Pils', abv: 4.3)
+puts 'Creating Data Base'
+Beer.create!(name: 'Heineken', country: 'Netherlands', style: 'Pils', abv: 4.5)
+Beer.create!(name: 'Kronenbourg', country: 'France', style: 'Pils', abv: 4.3)
 Beer.create!(name: 'Estrella', country: 'Spain', style: 'Pils', abv: 5)
 Beer.create!(name: 'Mahou', country: 'Spain', style: 'IPA', abv: 5.1)
 Beer.create!(name: 'Mad Goose', country: 'England', style: 'Pale Ale', abv: 4.7)
@@ -17,4 +17,4 @@ Beer.create!(name: 'Baltika', country: 'Russia', style: 'Golden Ale', abv: 4.2)
 Beer.create!(name: 'San Miguel', country: 'Spain', style: 'Pils', abv: 4.9)
 Beer.create!(name: 'Moritz', country: 'Spain', style: 'Pils', abv: 5.4)
 Beer.create!(name: 'Tsingtao', country: 'China', style: 'Pils', abv: 4)
-puts 'DB created'
+puts 'Data Base created'
